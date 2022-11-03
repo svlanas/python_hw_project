03865ff745a809b9436cd21b453280233b422771
Svitlana <svlana.shk@gmail.com>
2022-11-03 17:14:54 +0200

    First commit 5th homework

---
+++ hw 5/squares.py
@@ -0,0 +1,5 @@
+n = int(input("введіть число "))
+i = 0
+while i ** 2 < n:
+    print(i ** 2)
+    i += 1
---
+++ hw 5/while_0.py
@@ -0,0 +1,24 @@
+n = int(input("введіть число "))
+count = 0
+sum_ = 0
+med_ = 0
+max_ = n
+min_ = n
+count_even = 0
+count_odd = 0
+while n != 0:
+    count += 1
+    sum_ = sum_ + n
+    med = sum_ / count
+    if n > max_:
+        max_ = n
+    if n < min_:
+        min_ = n
+    if n % 2 == 0:
+        count_even += 1
+    else:
+        count_odd += 1
+    n = int(input("введіть ще число "))
+print("\n Сумма введених числе =", sum_, "\n Середнє арифметичне введених чисел =", med_,
+      "\n Максимальне значення серед введених чисел =", max_, "\n Мінімальне значення серед введених чисел =", min_,
+      "\n Кількість парних чисел =", count_even, "\n Кількість непарних чисел =", count_odd)
