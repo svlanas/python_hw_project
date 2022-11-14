8506654369d12488a9d206f0056c10c1f2b54824
Svitlana <svlana.shk@gmail.com>
2022-11-14 10:37:27 +0200

    fixed bugs in checking number of words

--- hw 8/sentence.py
+++ hw 8/sentence.py
@@ -1,9 +1,8 @@
 sentence = input("введіть речення з двох або більше слів ")
-while sentence.find(" ") == -1 or sentence.count(" ") < 1:
+while sentence.strip().find(" ") == -1 or sentence.strip().count(" ") < 1:
     print("ви ввели некоректне значення")
-    two_words = input("введіть речення з двох або більше слів ")
+    sentence = input("введіть речення з двох або більше слів ")
 sentence = sentence.expandtabs()
-# sentence = sentence.lower()
 sentence_lst = sentence.split(" ")
 n = sentence_lst.count("")
 for i in range(1, n+1):
