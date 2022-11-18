752b5cef06d9ee973b83477da4faa9a6063d0723
Svitlana <svlana.shk@gmail.com>
2022-11-18 10:51:47 +0200

    first commit 10th homework

--- hw 10/list_2.py
+++ hw 10/list_2.py
@@ -1,7 +1,7 @@
 import random
-numbers = len(
+number = len(
     set([random.randint(-10, 10) for i in range(10)])
     |
     set([random.randint(-10, 10) for j in range(10)])
 )
-print(numbers)
+print("Number of unique values in lists:", number)
